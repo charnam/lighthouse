38ac3da96d1f39d3deebe270411e3602e5a8d691
charnam <charnam@lunarsphere.net>
2025-08-18 13:57:07 -0400

    an attempt at fixing the "multiple groups" problem

--- server/authentication/permissions.js
+++ server/authentication/permissions.js
@@ -185,6 +185,7 @@ class Permissions {
 			FROM groups
 				LEFT JOIN user_group_relationships
 				ON user_group_relationships.groupid = groups.groupid
+				AND user_group_relationships.userid = ?
 			WHERE user_group_relationships.userid = ?
 			OR groups.userid = ?
 			ORDER BY user_group_relationships.position, user_group_relationships.id`,
